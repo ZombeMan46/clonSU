3abfda9ab971670f57a3920811a59c3c9cb10f22
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-18 15:52:31 +0200

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+end
+
+class Resume < ActiveRecord::Base
+   mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+   validates :name, presence: true # Make sure the owner's name is present.
 end
