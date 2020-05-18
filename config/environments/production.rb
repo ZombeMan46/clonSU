cb890a0b19ed42ac65cad93861123bc7899d7f4b
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-18 17:03:08 +0200

    mailer 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "projekt_#{Rails.env}"
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
   Rails.application.routes.default_url_options[:host] = 'suclon.herokuapp.com'
   config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
