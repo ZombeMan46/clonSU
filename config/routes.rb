e12505eca6ed8e6b577a438292311a9323b6fd51
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-18 18:02:04 +0200

    Celendar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'calendar/show'
+
   get 'resumes/index'
 
   get 'resumes/new'
@@ -20,4 +22,12 @@ end
 class Resume < ActiveRecord::Base
    mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
    validates :name, presence: true # Make sure the owner's name is present.
+end
+
+
+
+module CalendarHelper
+ def calendar(date = Date.today, &block)
+    Calendar.new(self, date, block).table
+  end
 end
