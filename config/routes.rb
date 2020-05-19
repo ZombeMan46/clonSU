46b80b4746b6c3b5e9c0b6f4c15f6d422a9c3b00
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-19 18:45:57 +0200

    revert 1

--- config/routes.rb
+++ config/routes.rb
@@ -15,17 +15,21 @@ Rails.application.routes.draw do
   get 'page/index'
   root to: 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-#  CarrierWaveExample::Application.routes.draw do
-#     resources :resumes, only: [:index, :new, :create, :destroy]
-#     root "resumes#index"
+#class Resume < ActiveRecord::Base
+#     mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+#     validates :name, presence: true # Make sure the owner's name is present.
 #  end
+
+
 
 
 
 end
 
-
+CarrierWaveExample::Application.routes.draw do
+   resources :resumes, only: [:index, :new, :create, :destroy]
+   root "resumes#index"
+end
 
 #celendar
 module CalendarHelper
