c058968bd4abeea746df45b6f5231f0aadb5031c
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-19 17:54:40 +0200

    meh

--- config/routes.rb
+++ config/routes.rb
@@ -15,10 +15,10 @@ Rails.application.routes.draw do
   get 'page/index'
   root to: 'page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-#class Resume < ActiveRecord::Base
-#     mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
-#     validates :name, presence: true # Make sure the owner's name is present.
-#  end
+class Resume < ActiveRecord::Base
+     mount_uploader :attachment, AttachmentUploader # Tells rails to use this uploader for this model.
+     validates :name, presence: true # Make sure the owner's name is present.
+  end
 
 
 
