5be283cfe281e7ac1b55faf905de32b2d53632a5
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-19 18:46:49 +0200

    rever 1.1

--- config/routes.rb
+++ config/routes.rb
@@ -26,10 +26,10 @@ Rails.application.routes.draw do
 
 end
 
-CarrierWaveExample::Application.routes.draw do
-   resources :resumes, only: [:index, :new, :create, :destroy]
-   root "resumes#index"
-end
+#CarrierWaveExample::Application.routes.draw do
+#   resources :resumes, only: [:index, :new, :create, :destroy]
+#   root "resumes#index"
+#end
 
 #celendar
 module CalendarHelper
