e712e0429467151b7e0e7e500c5bc8714ad1dccb
Žak Bršek <zak.bbrsek@gmail.com>
2020-05-19 18:35:51 +0200

    1

--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #  CarrierWaveExample::Application.routes.draw do
-     resources :resumes, only: [:index, :new, :create, :destroy]
-     root "resumes#index"
+#     resources :resumes, only: [:index, :new, :create, :destroy]
+#     root "resumes#index"
 #  end
 
 
